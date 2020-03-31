37f227ad3ea069bfb70416acaec414bc7e43240a
SirRuby <staffordharrison@protonmail.com>
2020-03-31 23:33:29 +0700

    clean up layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfully."
+      flash[:notice] = "Cookie was created successfully."
       redirect_to @article
     else
       render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully."
+      flash[:notice] = "Cookie was updated successfully."
       redirect_to @article
     else
       render 'edit'
@@ -36,6 +36,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
+    flash[:danger] = "You burnt a Cookie."
     redirect_to articles_path
   end
 
