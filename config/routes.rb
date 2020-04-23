1d4a2b410d56c77fbd9dcbe92f223b100ceceb81
SirRuby <staffordharrison@protonmail.com>
2020-04-23 23:14:58 +0700

    Show user info in articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+
+  get 'signup', to: 'users#new'
+  get 'users', to: 'users#create'
 end
