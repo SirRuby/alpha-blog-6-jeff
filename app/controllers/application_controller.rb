98f73c608fb6368e8ce0825906604b6c95f5cad7
SirRuby <staffordharrison@protonmail.com>
2020-04-25 03:05:38 +0700

    Create Controller methods as helper methods for current users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])  if session[:user_id]
+  end
+  #memoization to prevent multiple queries for current_user in the database
+
+  def logged_in?
+    !!current_user
+  end
+  #turn current_user instance variable into a true/false boolean to check if current user exists
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,16 +7,4 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
-
-def current_user
-  @current_user ||= User.find(session[:user_id])  if session[:user_id]
-
-end
-#memoization to prevent multiple queries for current_user in the database
-
-def logged_in?
-  !!current_user
-end
-#turn current_user instance variable into a true/false boolean to check if current user exists
-
 end
